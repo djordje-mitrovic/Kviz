f2243c82986bcb03911b962e66101c2eca94bf84
danica-mijajlovic <danicanajnaj@gmail.com>
2025-03-28 18:18:39 +0100

    Add files via upload

---
+++ client1.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8082")
+	if err != nil {
+		fmt.Println("Connection failed:", err)
+		return
+	}
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	receivedMessages := make([]string, 5) // Definišemo niz za čuvanje poruka
+
+	for i := 0; i < 5; i++ {
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error receiving message from server:", err)
+			return
+		}
+		receivedMessages[i] = msg // Čuvamo poruku u nizu
+	}
+
+	// Ispisujemo sve primljene poruke nakon što su sve sačuvane
+	i := 0
+	for _, msg := range receivedMessages {
+		if(i == 0){
+			fmt.Print("question", msg)
+		} else {
+			fmt.Print(i, ": ", msg)
+		}
+		i++
+	}
+
+
+	fmt.Print("Select a message to return: ")
+	userInput := bufio.NewReader(os.Stdin)
+	selectedMessage, _ := userInput.ReadString('\n')
+
+	// Proveravamo da li se unesena poruka nalazi u nizu receivedMessages
+	messageFound := false
+	for _, msg := range receivedMessages {
+		if selectedMessage == msg {
+			messageFound = true
+			break
+		}
+	}
+
+	// Ako je poruka pronađena u nizu, šaljemo je serveru, inače prijavljujemo grešku
+	if messageFound {
+		_, err := conn.Write([]byte(selectedMessage))
+		if err != nil {
+			fmt.Println("Error sending response to server:", err)
+		}
+	} else {
+		fmt.Println("Error: The selected message is not in the list.")
+	}
+}
---
+++ server1.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+type Server struct {
+	listenAddr string
+	ln         net.Listener
+	messages   []string
+}
+
+func NewServer(listenAddr string) *Server {
+	return &Server{
+		listenAddr: listenAddr,
+	}
+}
+
+func (s *Server) Start() error {
+	s.getMessagesFromUser()
+
+	ln, err := net.Listen("tcp", s.listenAddr)
+	if err != nil {
+		return err
+	}
+	defer ln.Close()
+	s.ln = ln
+	fmt.Println("Server is listening on", s.listenAddr)
+
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			fmt.Println("Accept error:", err)
+			continue
+		}
+
+		go s.handleClient(conn)
+	}
+}
+
+func (s *Server) getMessagesFromUser() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter 4 messages to send to the client:")
+	
+	fmt.Println("Enter the question:")
+	msg, _ := reader.ReadString('\n')
+	s.messages = append(s.messages, msg)
+	
+	for i := 0; i < 5; i++ {
+		fmt.Printf("Message %d: ", i+1)
+		msg, _ := reader.ReadString('\n')
+		s.messages = append(s.messages, msg)
+	}
+}
+
+func (s *Server) handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	for _, msg := range s.messages {
+		conn.Write([]byte(msg))
+		time.Sleep(1 * time.Second)
+	}
+
+	reader := bufio.NewReader(conn)
+	response, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading response from client:", err)
+		return
+	}
+
+	fmt.Println("Client selected:", response)
+	if(response == s.messages[5]) {
+		fmt.Println("Odgovor je tacan")
+	} else {
+		fmt.Println("Odgovor je netacan")
+	}
+}
+
+func main() {
+	server := NewServer(":8082")
+	log.Fatal(server.Start())
+}
