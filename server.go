89598c6b5a03e155c72ee215ac558ec766a7084c
danica-mijajlovic <danicanajnaj@gmail.com>
2025-04-09 11:32:59 +0200

    Add files via upload
    The server now generates 10 random questions from the database for each room that is created.

--- server.go
+++ server.go
@@ -2,28 +2,47 @@ package main
 
 import (
 	"bufio"
+	"database/sql"
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
-	//"os"
 	"strings"
-	//"time"
-	"database/sql"
+	"sync"
+	"time"
 	_ "github.com/mattn/go-sqlite3"
 )
+
+type Room struct {
+	code        string
+	clients     []net.Conn
+	questions   []string
+	answers     []string
+	options     [][]string
+}
 
 type Server struct {
 	listenAddr string
 	ln         net.Listener
-	questions  []string
-	answers    []string
-	options    [][]string
+	rooms      map[string]*Room
+	mu         sync.Mutex
 }
 
 func NewServer(listenAddr string) *Server {
 	return &Server{
 		listenAddr: listenAddr,
+		rooms:      make(map[string]*Room),
 	}
+}
+
+func (s *Server) generateRoomCode() string {
+	rand.Seed(time.Now().UnixNano())
+	letters := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	code := make([]rune, 5)
+	for i := range code {
+		code[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(code)
 }
 
 func (s *Server) Start() error {
@@ -31,142 +50,146 @@ func (s *Server) Start() error {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer db.Close()
 
-	s.getQuestions(db)
-	
-	ln, err := net.Listen("tcp", s.listenAddr)
+	s.ln, err = net.Listen("tcp", s.listenAddr)
 	if err != nil {
 		return err
 	}
-	defer ln.Close()
-	s.ln = ln
-	fmt.Println("Server je na adresi", s.listenAddr)
+	defer s.ln.Close()
+	fmt.Println("Server je pokrenut na", s.listenAddr)
 
 	for {
 		conn, err := s.ln.Accept()
 		if err != nil {
-			fmt.Println("Prihvati gresku:", err)
+			log.Println("Greška pri prihvatanju konekcije:", err)
 			continue
 		}
-
-		go s.handleClient(conn)
+		go s.handleConnection(conn, db)
 	}
 }
 
-func (s *Server) getQuestions(db *sql.DB) {
+func (s *Server) handleConnection(conn net.Conn, db *sql.DB) {
+	reader := bufio.NewReader(conn)
+	command, _ := reader.ReadString('\n')
+	command = strings.TrimSpace(command)
+
+	if command == "CREATE_ROOM" {
+		roomCode := s.generateRoomCode()
+		s.mu.Lock()
+		// Kreiranje sobe sa pitanjima iz baze
+		questions, options, answers := s.getQuestionsForRoom(db)
+		s.rooms[roomCode] = &Room{
+			code:      roomCode,
+			clients:   []net.Conn{conn},
+			questions: questions,
+			options:   options,
+			answers:   answers,
+		}
+		s.mu.Unlock()
+		conn.Write([]byte("ROOM_CODE " + roomCode + "\n"))
+	} else if strings.HasPrefix(command, "JOIN_ROOM") {
+		parts := strings.Split(command, " ")
+		if len(parts) != 2 {
+			conn.Write([]byte("INVALID_COMMAND\n"))
+			return
+		}
+		roomCode := parts[1]
+		s.mu.Lock()
+		room, exists := s.rooms[roomCode]
+		if !exists {
+			conn.Write([]byte("ROOM_NOT_FOUND\n"))
+			s.mu.Unlock()
+			return
+		}
+		if len(room.clients) >= 2 {
+			conn.Write([]byte("ROOM_FULL\n"))
+			s.mu.Unlock()
+			return
+		}
+		room.clients = append(room.clients, conn)
+		if len(room.clients) == 2 {
+			go s.startGame(room)
+		}
+		s.mu.Unlock()
+		conn.Write([]byte("JOINED_ROOM\n"))
+	} else {
+		conn.Write([]byte("UNKNOWN_COMMAND\n"))
+	}
+}
+
+func (s *Server) getQuestionsForRoom(db *sql.DB) ([]string, [][]string, []string) {
+	var questions []string
+	var options [][]string
+	var answers []string
+
 	rows, err := db.Query("SELECT pitanja, odgovor1, odgovor2, odgovor3, odgovor4, tacan_odgovor FROM questions ORDER BY RANDOM() LIMIT 10;")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	//pitanje1, a, b, c, d, 1
-	//pitanje2, a, b, c, d, 2
-
 	for rows.Next() {
-		var pitanje string
-		var opcija1, opcija2, opcija3, opcija4 string
+		var pitanje, o1, o2, o3, o4 string
 		var odgovor int
-
-		// Skeniraj podatke iz trenutnog reda u promenljive
-		if err := rows.Scan(&pitanje, &opcija1, &opcija2, &opcija3, &opcija4, &odgovor); err != nil {
-			log.Fatal(err)	
+		if err := rows.Scan(&pitanje, &o1, &o2, &o3, &o4, &odgovor); err != nil {
+			log.Fatal(err)
 		}
-		
-		s.questions = append(s.questions, pitanje)
-		s.options = append(s.options, []string{opcija1, opcija2, opcija3, opcija4})
-		s.answers = append(s.answers, fmt.Sprintf("%d", odgovor))
+		questions = append(questions, pitanje)
+		options = append(options, []string{o1, o2, o3, o4})
+		answers = append(answers, fmt.Sprintf("%d", odgovor))
 	}
 	if err := rows.Err(); err != nil {
-		log.Fatal("Greska pri obradi podataka iz baze.")
+		log.Fatal(err)
 	}
-	fmt.Println("Pitanja su uspesno ucitana.")
-
+	return questions, options, answers
 }
 
+func (s *Server) startGame(room *Room) {
+	p1 := room.clients[0]
+	p2 := room.clients[1]
+	defer p1.Close()
+	defer p2.Close()
 
-// CTRL + K + C
-// func (s *Server) getMessagesFromUser() {
-// 	//reader := bufio.NewReader(os.Stdin)
-
-// 	//fmt.Println("Unesi 10 pitanja, 4 opcije po pitanju i tacan odgovor:")
-
-// 	// Unos pitanja
-// 	for i := 0; i < 3; i++ {
-// 		fmt.Printf("Unesi pitanje %d: ", i+1)
-// 		msg, _ := reader.ReadString('\n')
-// 		s.questions = append(s.questions, msg)
-
-// 		// Unos ponuđenih odgovora
-// 		options := make([]string, 4)
-// 		for j := 0; j < 4; j++ {
-// 			fmt.Printf("Unesi opciju %d za pitanje %d: ", j+1, i+1)
-// 			option, _ := reader.ReadString('\n')
-// 			options[j] = option
-// 		}
-// 		s.options = append(s.options, options)
-
-// 		// Unos tačnog odgovora
-// 		fmt.Printf("Unesi broj tacnog odgovora (1-4) za pitanje %d: ", i+1)
-// 		var correctAnswer int
-// 		_, err := fmt.Scanf("%d\n", &correctAnswer)
-// 		if err != nil {
-// 			fmt.Println("Pogresan ulaz")
-// 		}
-// 		// Dodajemo odgovarajući ponuđeni odgovor
-// 		s.answers = append(s.answers, fmt.Sprintf("%d", correctAnswer))
-// 	}
-// }
-
-func (s *Server) handleClient(conn net.Conn) {
-	defer conn.Close()
-
-	poeni := 0
-
-	// Interaktivno slanje pitanja i čekanje odgovora
-	for i := 0; i < len(s.questions); i++ {
-		// Šaljemo pitanje klijentu
-		conn.Write([]byte(s.questions[i] + "\n"))
-		//time.Sleep(1 * time.Second)
-
-		// Šaljemo ponuđene odgovore
-		for _, option := range s.options[i] {
-			conn.Write([]byte(fmt.Sprintf("%s\n", option)))
+	r1 := bufio.NewReader(p1)
+	r2 := bufio.NewReader(p2)
+	s1, s2 := 0, 0
+	for i, q := range room.questions {
+		p1.Write([]byte(q + "\n"))
+		p2.Write([]byte(q + "\n"))
+		for _, opt := range room.options[i] {
+			p1.Write([]byte(opt + "\n"))
+			p2.Write([]byte(opt + "\n"))
 		}
-
-		// Čekamo odgovor od klijenta
-		reader := bufio.NewReader(conn)
-		response, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Greska prilikom citanja odgovora klijenta:", err)
-			return
+		resp1, _ := r1.ReadString('\n')
+		resp2, _ := r2.ReadString('\n')
+		resp1 = strings.TrimSpace(resp1)
+		resp2 = strings.TrimSpace(resp2)
+		msg1, msg2 := "Odgovor je netačan\n", "Odgovor je netačan\n"
+		if resp1 == room.answers[i] {
+			s1++
+			msg1 = "Odgovor je tačan\n"
 		}
-
-		// Proveravamo tačnost odgovora
-		tacno := "Odgovor je netačan\n"
-		//num, _ := strconv.Atoi(response)
-
-		//fmt.Printf("%s se poredi sa %s\n", strings.TrimSpace(response), s.answers[i])
-		if strings.TrimSpace(response) == s.answers[i] {
-			tacno = "Odgovor je tačan\n"
-			poeni+=1
+		if resp2 == room.answers[i] {
+			s2++
+			msg2 = "Odgovor je tačan\n"
 		}
-
-		// Šaljemo rezultat klijentu
-		_, err = conn.Write([]byte(tacno))
-		if err != nil {
-			fmt.Println("Greska prilikom slanja odgovora klijentu:", err)
-			return
-		}
+		p1.Write([]byte(msg1))
+		p2.Write([]byte(msg2))
 	}
 
-	conn.Write([]byte(fmt.Sprintf("%d\n", poeni)))
-
+	if s1 > s2 {
+		p1.Write([]byte(fmt.Sprintf("Pobedio si sa %d poena!\n", s1)))
+		p2.Write([]byte(fmt.Sprintf("Izgubio si sa %d poena!\n", s2)))
+	} else if s2 > s1 {
+		p2.Write([]byte(fmt.Sprintf("Pobedio si sa %d poena!\n", s2)))
+		p1.Write([]byte(fmt.Sprintf("Izgubio si sa %d poena!\n", s1)))
+	} else {
+		p1.Write([]byte(fmt.Sprintf("Nereseno! Osvojio si %d poena!\n", s1)))
+		p2.Write([]byte(fmt.Sprintf("Nereseno! Osvojio si %d poena!\n", s2)))
+	}
 }
 
 func main() {
-
 	server := NewServer(":8082")
 	log.Fatal(server.Start())
 }
+
